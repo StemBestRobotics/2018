0e6343d6cc43201a2f8667d2d114de14e2693557
Avargish <42498436+Avargish@users.noreply.github.com>
2018-10-13 16:35:02 -0600

    Movement + arm
    Added arm
    Positioned arm base
    Implemented movement for base (arrow keys)
    Implemented rotation for arm base (A and D)
    Added Colliders for base, gamefield, and slider

---
+++ 2018 Best/Assets/ArmRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * 30);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * 30);
+        }
+    }
+}
---
+++ 2018 Best/Assets/Drive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += transform.forward * 1 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position -= transform.forward * 1 * Time.deltaTime;
+        }
+    }
+}
