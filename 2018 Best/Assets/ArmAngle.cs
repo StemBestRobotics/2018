37841c67facfc892011961208f20a3a640c14e0d
Avargish <42498436+Avargish@users.noreply.github.com>
2018-10-27 16:04:57 -0600

    Import, Position, Code
    Imported all of the V2 Models
    Positioned all of the V2 Models
    Added Terrain
    Added collider for claw
    Added code/controls for angling the arm

---
+++ 2018 Best/Assets/ArmAngle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmAngle: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * 30);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * 30);
+        }
+    }
+}
