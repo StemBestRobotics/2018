bdaa923a3948a8e83c5c4f3b938cb03dfcd24f23
Avargish <42498436+Avargish@users.noreply.github.com>
2018-11-09 20:55:14 -0700

    Toggle??????
    Toggle that is in progress but sorta works

--- 2018 Best/Assets/robotBehaviourScript.cs
+++ 2018 Best/Assets/robotBehaviourScript.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class robotBehaviourScript : MonoBehaviour {
     public GameObject robotArm;
@@ -12,7 +10,10 @@ public class robotBehaviourScript : MonoBehaviour {
     public GameObject robotClawGrab;
 
     public float speed = 50;
-    public float speedtest = 0.5f;
+
+    public bool clawRotateCheck = true;
+    public bool clawRotateToggle = true;
+    public float clawRotateValue = 0;
 
     // Use this for initialization
     void Start () {
@@ -26,9 +27,32 @@ public class robotBehaviourScript : MonoBehaviour {
         robotArmHousing.transform.Rotate(0,0,Input.GetAxis("robotArmAngle") * Time.deltaTime * speed * 0.4f);
         robotArmExtension.transform.localPosition += new Vector3(Input.GetAxis("robotArmExtension") * Time.deltaTime * speed * 0.04f, 0, 0);
         robotArmTelescope.transform.localPosition += new Vector3(Input.GetAxis("robotArmExtension") * Time.deltaTime * speed * 0.04f, 0, 0);
-        robotClawBase.transform.Rotate(0, Input.GetAxis("robotClawRotate") * Time.deltaTime * speed, 0);
+        if (Input.GetAxis("robotClawRotate") > 0 && clawRotateCheck) {
+            if (clawRotateToggle == true){
+                if (clawRotateValue < 90) {
+                    robotClawBase.transform.Rotate(0, 10 * Time.deltaTime, 0);
+                    clawRotateValue += 10 * Time.deltaTime;
+                } else {
+                    clawRotateToggle = false;
+                    clawRotateCheck = true;
+                }
+            } else {
+                if (clawRotateValue > 0)
+                {
+                    robotClawBase.transform.Rotate(0, -10 * Time.deltaTime, 0);
+                    clawRotateValue -= 10 * Time.deltaTime;
+                }
+                else
+                {
+                    clawRotateToggle = true;
+                    clawRotateCheck = true;
+                }
+            }
+        }
         robotClawWrist.transform.Rotate(0, 0, Input.GetAxis("robotClawAngle") * Time.deltaTime * speed);
         robotClawGrab.transform.Rotate(0, 0, Input.GetAxis("robotClawGrab") * Time.deltaTime * speed );
+        Debug.Log(clawRotateValue);
+        Debug.Log(Input.GetAxis("robotClawRotate"));
         /*
         if (Input.GetKey(KeyCode.D))
         {
